302c2f31081b838e8a55c803147a3df79a62b4ac
prijacash <prijacash@gmail.com>
2022-10-07 15:23:14 -0700

    stubbed out course components

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div>
+            <p>This is Home</p>
+        </div>
+    )
+}
---
+++ src/components/pages/NewCourse.jsx
@@ -0,0 +1,7 @@
+export default function NewCourse(){
+    return(
+        <div>
+            <p>This is NewCourse</p>
+        </div>
+    )
+}
