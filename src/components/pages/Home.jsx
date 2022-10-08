d11b8b7119ae13d7259439126596187e9ec87e5d
prijacash <prijacash@gmail.com>
2022-10-07 17:00:49 -0700

    updated home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,49 @@
+import axious from 'axios'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
 export default function Home(){
+    // courses from the backend
+    const [courses, setCourses] = useState([])
+    // state for messages from backend
+    const [errorMessage, setErrorMessage] = useState('')
+
+    console.log('server url', process.env.REACT_APP_SERVER_URL)
+
+    useEffect(() => {
+        const getCourses = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/course`)
+                setCourses(response.data)  
+            } catch(err) {
+                console.warn(err)
+                if (err.response) {
+                    setErrorMessage(err.response.data.message)   
+                }
+            }
+        }
+        
+        getCourses()
+    }, [])  // only fire on page load
+    
+    const courseLinks = courses.map(course => {
+        return (
+            <div key={course._id}>
+                <Link to={`/courses/${course._id}`}>{course.name}</Link>
+            </div>
+        )
+})
+
     return(
         <div>
-            <p>This is Home</p>
+            <h1>Welcome to the Bloom App</h1>
+
+            <h1>INSERT SLIDER CAROUSEL SLIDER</h1>
+
+            {courseLinks}
+
+            <p>{errorMessage}</p>
         </div>
     )
 }
