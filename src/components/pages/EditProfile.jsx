1f29498619b5545e789186a6ea1f734de7711d30
iandab47 <dabinett.ian@gmail.com>
2022-10-09 14:34:32 -0500

    adds axios call for user details

--- src/components/pages/EditProfile.jsx
+++ src/components/pages/EditProfile.jsx
@@ -1,7 +1,24 @@
-export default function EditProfile(){
-  return(
-      <div>
-          <p>This is EditProfile</p>
-      </div>
+import axios from "axios"
+import { useEffect } from "react"
+
+export default function EditProfile({ currentUser, setCurrentUser }) {
+  // State
+
+  // Hooks
+  useEffect(() => {
+    const getUserDetails = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${currentUser.id}`)
+
+      console.log(response.data)
+    }
+    getUserDetails()
+  }, [])
+
+  return (
+    <div>
+        <h1>This is EditProfile</h1>
+
+
+    </div>
   )
 }
