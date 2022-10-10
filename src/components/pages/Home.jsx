01cf93a01daf77d8d894e11117154f77b86648d2
JasonXChen <jasonxchen888@gmail.com>
2022-10-09 23:08:05 -0400

    Move prija's carousel into partial component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Carousel from "../partials/Carousel";
 
 export default function Home(){
     // courses from the backend
@@ -42,124 +43,7 @@ export default function Home(){
             <h1 className="self-center">Welcome to the Bloom App</h1>
 
             <h1 className="self-center">INSERT SLIDER CAROUSEL SLIDER</h1>
-
-            <div id="carouselExampleCaptions" className="carousel slide relative w-full" data-bs-ride="carousel">
-                    <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-                        <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide-to="0"
-                        className="active"
-                        aria-current="true"
-                        aria-label="Slide 1"
-                        ></button>
-                        <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide-to="1"
-                        aria-label="Slide 2"
-                        ></button>
-                        <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide-to="2"
-                        aria-label="Slide 3"
-                        ></button>
-                        <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide-to="3"
-                        aria-label="Slide 4"
-                        ></button>
-                        <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide-to="4"
-                        aria-label="Slide 5"
-                        ></button>
-                    </div>
-
-                    <div className="carousel-inner relative w-full overflow-hidden">
-                        <div className="carousel-item active relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/Nd7b6v8.png"
-                                className="block w-full h-96 object-cover align-middle"
-                                alt="..."
-                            />
-                            <div className="carousel-caption hidden md:block absolute text-center">
-                                <h5 className="text-xl">Decorated Events</h5>
-                                <p>Learn how to spark joy with Curated Experiences</p>
-                            </div>
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/PGdtGHh.png"
-                                className="block w-full h-96 object-cover align-middle"
-                                alt="..."
-                            />
-                            <div className="carousel-caption hidden md:block absolute text-center">
-                                <h5 className="text-xl">Learn how to Start a Clothing Brand</h5>
-                                <p>10 years of Streetwear Brand Experience</p>
-                            </div>
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/ICK45WM.png"
-                                className="block w-full h-96 object-cover align-middle"
-                                alt="..."
-                            />
-                            <div className="carousel-caption hidden md:block absolute text-center">
-                                <h5 className="text-xl">Learn how to be an AirBNB Host</h5>
-                                <p>Lead with passion and authenticity, then then business aspect will follow.</p>
-                            </div>
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/TrKJPFd.png"
-                                className="block w-full h-96 object-cover align-middle"
-                                alt="..."
-                            />
-                            <div className="carousel-caption hidden md:block absolute text-center">
-                                <h5 className="text-xl">Scuba Diving Intro</h5>
-                                <p>Learn what it takes to get certified as a diver</p>
-                            </div>
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/Qnp6Y0v.png"
-                                className="block w-full h-96 object-cover align-middle"
-                                alt="..."
-                            />
-                            <div className="carousel-caption hidden md:block absolute text-center">
-                                <h5 className="text-xl">Learn to Become a Hair Stylist</h5>
-                                <p>Learn the basics of the Hair Styling Business.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <button
-                        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="prev"
-                    >
-                        <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="next"
-                    >
-                        <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
+            <Carousel />
 
             <h3 className="font-medium">Featured</h3>
             <div className="flex gap-8">
