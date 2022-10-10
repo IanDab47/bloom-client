7b746b0c92c0ae028bc0a1303b997c4b0b2ba715
JasonXChen <jasonxchen888@gmail.com>
2022-10-09 23:15:45 -0400

    Switch rendering to use existing partials

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import Carousel from "../partials/Carousel";
+import CourseList from "../partials/CourseList";
 
 export default function Home(){
     // courses from the backend
@@ -13,9 +13,6 @@ export default function Home(){
         const getCourses = async () => {
             try {
                 const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/courses`)
-
-                console.log(response)
-
                 setCourses(response.data)  
             } catch(err) {
                 console.warn(err)
@@ -28,16 +25,6 @@ export default function Home(){
         getCourses()
     }, [])  // only fire on page load
 
-    console.log(courses)
-    
-    const courseLinks = courses.map(course => {
-        return (
-            <div key={course._id} className="border-2 px-4 py-3">
-                <Link to={`/courses/${course._id}`}>{course.title}</Link>
-            </div>
-        )
-})
-
     return(
         <div className="flex flex-col items-start">
             <h1 className="self-center">Welcome to the Bloom App</h1>
@@ -45,10 +32,8 @@ export default function Home(){
             <h1 className="self-center">INSERT SLIDER CAROUSEL SLIDER</h1>
             <Carousel />
 
-            <h3 className="font-medium">Featured</h3>
-            <div className="flex gap-8">
-                {courseLinks}
-            </div>
+            <h3 className="mt-8 text-xl font-medium">Featured</h3>
+            <CourseList courses={courses} />
 
             <p>{errorMessage}</p>
         </div>
