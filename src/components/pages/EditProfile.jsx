65695cbaa9f718a9debf7523ef50fd139350f527
iandab47 <dabinett.ian@gmail.com>
2022-10-09 20:51:24 -0500

    resolves react bug

--- src/components/pages/EditProfile.jsx
+++ src/components/pages/EditProfile.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 import axios from "axios"
 
 export default function EditProfile({ currentUser, handleLogout }) {
   // State
+  const { userId } = useParams()
   const [userDetails, setUserDetails] = useState({
     name: '',
     email: '',
@@ -16,7 +18,7 @@ export default function EditProfile({ currentUser, handleLogout }) {
   useEffect(() => {
     const getUserDetails = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${currentUser.id}`)
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${userId}`)
   
         setUserDetails(response.data)
       } catch (err) {
@@ -33,11 +35,33 @@ export default function EditProfile({ currentUser, handleLogout }) {
     getUserDetails()
   }, [])
 
+  // Handler
+  const handleChange = (e, name) => {
+    console.log(e.target.value, userDetails)
+  }
+
   return (
     <div>
         <h1>This is EditProfile</h1>
 
+        <form>
+          
+          <div>
+            <label htmlFor="name">Enter your new username: </label>
+            <input type='text' id="name" name="name" value={userDetails.name} onChange={e => handleChange(e, 'name')} />
+          </div>
 
+          <div>
+            <label htmlFor="email">Enter your new email: </label>
+            <input type="email" id="email" name="email" value={userDetails.email} onChange={e => handleChange(e, 'email')} />
+          </div>
+
+          <div>
+            <label htmlFor="password">Enter you new password: </label>
+            <input type="password" id="password" name="password" value={userDetails.password} onChange={e => handleChange(e, 'password')} />
+          </div>
+
+        </form>
     </div>
   )
 }
