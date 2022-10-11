814de3daf8c0d45b0e093246cbdadded74f1df51
Agrate071818 <zionagrate@gmail.com>
2022-10-11 17:49:06 -0400

    CouseCard

--- src/components/partials/CourseCard.js
+++ src/components/partials/CourseCard.js
@@ -12,13 +12,21 @@ function CourseCard({course}) {
         getUser();
     });
     return (
-        <Link to={`/courses/${course._id}`} >
-            <div className="flex flex-col border-2 px-4 py-3">
-                    <p className="font-semibold">{course.title}</p>
-                    <p className="text-gray-500">{username}</p>
-                    <p className="font-semibold">${course.price}</p>
-            </div>
-        </Link>
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md bg-[#b9c1a6] ">
+            <Link to={`/courses/${course._id}`} >
+                <img class="rounded-t-lg" src={course.photoLink} alt="" />
+            </Link>
+        <div class="p-5">
+            <Link to={`/courses/${course._id}`} >
+                <h5 class="mb-2 text-2xl font-bold font-bloom-sans italic  tracking-tight text-bloom-grey ">{course.title}</h5>
+            </Link>
+            <p class="mb-3 font-normal  font-bloom-sans text-bloom-grey">{username}</p>
+            <Link to={`/courses/${course._id}`} class="inline-flex items-center py-2 px-3 text-sm font-medium font-bloom-sans text-center text-white rounded-lg bg-[#898e59] hover:bg-[#aab161]">
+                Learn more
+                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </Link>
+        </div>
+    </div>
     );
 }
 
