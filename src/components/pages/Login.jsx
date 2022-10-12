21f3de2712e23db0cc04482fef26e50d96b80990
prijacash <prijacash@gmail.com>
2022-10-12 10:09:52 -0700

    updated login colors

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -52,7 +52,7 @@ export default function Login({ currentUser, setCurrentUser }) {
 					<div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
 						<div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
 							<img
-							src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+							src="/loginNew.svg"
 							class="w-full"
 							alt="Phone image"
 							/>
@@ -64,7 +64,7 @@ export default function Login({ currentUser, setCurrentUser }) {
 								<br></br>
 
 								{/* <!-- Email input --> */}
-								<div class="mb-6">
+								<div class="mb-6 ">
 									<input 
 										type="email"
 										id="email"
@@ -90,7 +90,7 @@ export default function Login({ currentUser, setCurrentUser }) {
 								{/* <!-- Submit button --> */}
 								<button
 									type="submit"
-									class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+									class="inline-block px-7 py-3 bg-bloom-olive text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
 									data-mdb-ripple="true"
 									data-mdb-ripple-color="light"
 								>
