33d8aecb99ab520b961323cd2cbda23b0e875311
iandab47 <dabinett.ian@gmail.com>
2022-10-12 11:47:53 -0500

    gives carousel fixed width

--- src/components/partials/Carousel.js
+++ src/components/partials/Carousel.js
@@ -1,6 +1,6 @@
 function Carousel({ isWide }) {
     return (
-        <div id="carouselExampleCaptions" className={`carousel slide relative w-full max-w-[75rem] drop-shadow-lg ${isWide ? 'float-right' : ''}`} data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className={`carousel slide relative w-full max-w-[53rem] drop-shadow-lg ${isWide ? 'float-right' : ''}`} data-bs-ride="carousel">
             <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-2">
                 <button
                 type="button"
