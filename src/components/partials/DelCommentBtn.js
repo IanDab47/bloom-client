2db82740bdc55f72e3e1e385fe5ceeaaf89755e3
prijacash <prijacash@gmail.com>
2022-10-12 20:41:34 -0700

    centered cards

--- src/components/partials/DelCommentBtn.js
+++ src/components/partials/DelCommentBtn.js
@@ -2,7 +2,7 @@ function DelCommentBtn({comment, currentUser, handleCommentDelete}) {
     return (
         <div>{" | "}
             {currentUser && currentUser.id === comment.commenter._id ? 
-                <button className="text-gray-300 italic hover:text-red-600" onClick={() => handleCommentDelete(comment._id)}>
+                <button className="text-gray-600 italic hover:text-red-600" onClick={() => handleCommentDelete(comment._id)}>
                     Delete
                 </button>
                 :
