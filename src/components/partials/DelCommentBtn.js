504a2a3ecb965df6d0f5170d8dacd0255a7a3cd7
prijacash <prijacash@gmail.com>
2022-10-12 20:16:51 -0700

    updated delete button

--- src/components/partials/DelCommentBtn.js
+++ src/components/partials/DelCommentBtn.js
@@ -1,8 +1,8 @@
 function DelCommentBtn({comment, currentUser, handleCommentDelete}) {
     return (
-        <div>
+        <div>{" | "}
             {currentUser && currentUser.id === comment.commenter._id ? 
-                <button className="bg-red-500 text-white" onClick={() => handleCommentDelete(comment._id)}>
+                <button className="text-gray-300 italic hover:text-red-600" onClick={() => handleCommentDelete(comment._id)}>
                     Delete
                 </button>
                 :
